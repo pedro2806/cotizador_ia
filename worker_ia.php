29dab81884006e5d80dab67d4ee9b8dd731ca725
pedro2806 <isc.pedro.mtz@gmail.com>
2026-05-04 15:15:02 -0600

    Update worker_ia.php

--- worker_ia.php
+++ worker_ia.php
@@ -194,7 +194,7 @@ while (true) {
     }
 
     $data = [
-        "cdmess" => $cdmess_para_aprendizaje,
+        "cdmess" => $cdmess_para_aprendizaje ?? $respuesta_ia['cdmess'] ?? 'N/A',
         "desc" => $item['descripcion_historica'] ?? $entrada,
         "precio_min" => round($stats['min'] ?? 0, 2),
         "precio_max" => round($stats['max'] ?? 0, 2),
